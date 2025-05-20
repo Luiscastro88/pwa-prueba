d0305507f7ff204bc956841d43d71aabc6e8a61a
Luiscastro88 <luiscastroipp2@gmail.com>
2025-05-20 12:22:56 -0500

    Add files via upload

---
+++ pwa-test/script.js
@@ -0,0 +1,29 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById('installBtn').hidden = false;
+});
+
+document.getElementById('installBtn').addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === 'accepted') {
+      console.log('App instalada');
+    }
+    deferredPrompt = null;
+    document.getElementById('installBtn').hidden = true;
+  }
+});
+
+document.getElementById('redirectBtn').addEventListener('click', () => {
+  window.location.href = "https://www.gob.pe/satsullana";
+});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registrado'))
+    .catch(err => console.error('Error al registrar el Service Worker:', err));
+}
